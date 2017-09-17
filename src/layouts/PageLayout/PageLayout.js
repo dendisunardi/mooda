aa4e02fcad76c311d1d562672eb71fa8ba9636ab
dendi-toped <dendi.sunardi@tokopedia.com>
2017-09-17 23:32:07 +0700

    test server node

--- src/layouts/PageLayout/PageLayout.js
+++ src/layouts/PageLayout/PageLayout.js
@@ -2,22 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import './PageLayout.scss'
 import 'bulma-scss'
-import Navbar from './../../components/Navbar'
-// import Opener from './../../components/Opener'
-// import RegisterPanel from './../../components/RegisterPanel'
-import Footer from './../../components/Footer'
 
 export const PageLayout = ({ children }) => (
   <div className='layout-default'>
-    {
-      // <Navbar />
-    }
     <div className='page-layout__viewport content u-flex-auto'>
       {children}
     </div>
-    {
-      // <Footer />
-    }
   </div>
 )
 PageLayout.propTypes = {
